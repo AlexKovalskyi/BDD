d323f7f280817bba7101ed535ba434b76633fb8c
Aleksandr Kovalskyi <alex.kowalskiy@gmail.com>
2017-06-23 16:17:10 +0300

    Added feature

---
+++ Features/SpecFlowDemo.feature
@@ -0,0 +1,24 @@
+﻿Feature: SpecFlowDemo
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@SmokeTest
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+
+Scenario: Create a new employee with mandatary details
+	#Given I have opend my application
+	#Then Ishould see employee detais page
+	When Ifill all the mandatory detail in form
+	| Name    | Age | Phone         | Email             |
+	| kartrik | 28  | 80631089758   | kartrik@gmail.com |
+	| jon     | 45  | 8063108323338 | jhon@gmail.com    |
+	| sam     | 33  | 8063108332432 | samk@gmail.com    |
+
+	#And I click the save button
+	#Then Ishoul see all the details saved in my application and DB
---
+++ StepDefinitions/SampleFeatureSteps.cs
@@ -0,0 +1,55 @@
+﻿using SpecflowDemo.StepDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecflowDemo.StepDefinitions
+{
+    [Binding]
+    class SampleFeatureSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            Console.WriteLine(number);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Pressed add button.");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 120)//Grab the object whick has the value 120 un the UI and replace this
+            {
+                Console.WriteLine("the test passed");
+            }
+            else
+            {
+                Console.WriteLine("The test FAILED");
+            }
+        }
+
+        [When(@"Ifill all the mandatory detail in form")]
+        public void WhenIfillAllTheMandatoryDetailInForm(Table table)
+        {
+            var details = table.CreateSet<EmployeeDetails>();
+
+            foreach (var item in details)
+            {
+
+                Console.WriteLine(item.Age);
+                Console.WriteLine(item.Email);
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Phone);
+            }
+        }
+    }
+}
